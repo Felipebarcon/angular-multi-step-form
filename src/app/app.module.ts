b29602a41dc590c8c7ecbb3939d7c527b96b43af
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-08 11:37:46 +0100

    starting html and css layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormContainerComponent } from './form-container/form-container.component';
+import { StepComponent } from './form-container/step/step.component';
+import { PersonalInfoComponent } from './form-container/personal-info/personal-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormContainerComponent,
+    StepComponent,
+    PersonalInfoComponent
   ],
   imports: [
     BrowserModule
